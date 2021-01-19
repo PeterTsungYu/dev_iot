2e9662e2f67d9a8bcea96ef5c82dd4a138865aef
Peter_Rpi <yoyo5022@gmail.com>
2021-01-19 17:46:46 +0800

    gpio script

--- gpio.py
+++ gpio.py
@@ -1,9 +1,41 @@
+# %% 
 import RPi.GPIO as GPIO
+import time
 
+print('Succeed')
+
+# %%
 # GPIO nymbering 
 GPIO.setmode(GPIO.BCM)
 
-# pin number variable 
-TX = 14
-RX = 15
+print('Succeed')
 
+# %%
+# read High as 3.3V
+channel_DGM = 18
+
+try:
+    # make sure it is not a float port
+    GPIO.setup(channel_DGM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    #GPIO.setup(channel_DGM, GPIO.IN) 
+    print(GPIO.input(channel_DGM)) # while a >=1.6V(3.3/2) input to the pin, it will detect as 1
+
+except Exception as ex:
+    print ("Input port is not High" + str(ex))
+    exit()
+
+# %%
+try:
+    GPIO.wait_for_edge(channel_DGM, GPIO.RISING)  
+    while True:            # this will carry on until you hit CTRL+C  
+        if GPIO.input(channel_DGM): 
+            print("Channel_DGM is 1")  
+        else:  
+            print("Channel_DGM is 0")
+        time.sleep(0.1) 
+  
+except KeyboardInterrupt:  
+    GPIO.cleanup()          
+# %%
+GPIO.cleanup() 
+# %%
