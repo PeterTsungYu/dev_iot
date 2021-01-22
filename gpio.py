a5e76590a76d09267b6610c5af828beac80e4bec
Peter_Rpi <yoyo5022@gmail.com>
2021-01-22 17:37:16 +0800

    DFC exp succeed

--- gpio.py
+++ gpio.py
@@ -38,16 +38,18 @@ try:
     GPIO.add_event_detect(channel_DGM, GPIO.RISING, callback=read)
     print('Port setup')
 
+    input("Press Enter when ready\n>")
+    print("Start now...")  
     start = time.time()
     flag_time = start
-    #if ticker_2.wait(WAIT_TIME_SECONDS * 5):    
+
     while not ticker.wait(WAIT_TIME_SECONDS):    
         flag_min()
         print(f"Flag time is {time.time() - flag_time}")
         flag_time = time.time()
         
         count += 1
-        if count == 3: # record for mins
+        if count == 5: # record for mins
             break
         
 
@@ -59,9 +61,6 @@ except Exception as ex:
     print ("communicating error " + str(ex))    
     print("=="*30)
 finally:
-    #print(pulse_times_min)
-    #print([len(i) for i in pulse_times_min])
-    #print(pulse_times)
     print("Clean up ports...")  
     GPIO.cleanup()
     print("=="*30)  
@@ -69,36 +68,34 @@ finally:
 # %%
 print("Data Analysis...")
 
-average_flow_rates_min = []
+average_min_lst = []
 
-for pulse_times in pulse_times_min:
-    average_flow_rates = []
+for pulse_times in pulse_times_min: # [[1st min pulse_times], [2nd min pulse_times], [3rd min pulse_times], ...]
+    average_interval_lst = []
 
-    for interval in range(5, 55, 5):
+    for interval in range(5, 55, 5): # [[1st min intervals], [2nd min intervals], [3rd min intervals], ...]
         #print(interval)
-        flow_rate_lst = [] 
+        flow_rate_interval_lst = [] 
 
+        # for each interval, calculate the average flow rate
         for i in range(interval, len(pulse_times), interval):
             try:
-                #print(f"this is the {i}")
                 # flow rate in [liter/s]
                 # 0.1 liter / pulse
                 flow_rate = 60 * 0.1 * (interval-1) / (pulse_times[i-1] - pulse_times[i-interval])
-                flow_rate_lst.append(round(flow_rate, 2)) 
+                flow_rate_interval_lst.append(round(flow_rate, 2)) 
             except Exception as ex:
                 print("Error: " + str(ex))
 
-        #print(f"In each set-up interval ({interval}[s]), the flow rates are calculated as:\n{flow_rate_lst} in liter/s")
-        average_flow_rate = round(sum(flow_rate_lst) / len(flow_rate_lst), 2)
-        #print(f"During the period of {pulse_times[interval*int(len(pulse_times)/interval) - 1] - pulse_times[0]}[s], the average flow rate is:\n{average_flow_rate} [liter/s]")
-        #print("-"*30)
+        average_flow_rate_interval = round(sum(flow_rate_interval_lst) / len(flow_rate_interval_lst), 2)
+        #print(f"In each set-up interval ({interval}[s]), the flow rates are calculated as:\n{flow_rate_interval_lst} in liter/s")
+        #print(f"During the period of {pulse_times[interval*int(len(pulse_times)/interval) - 1] - pulse_times[0]}[s], the average flow rate is:\n{average_flow_rate_interval} [liter/s]")
 
-        average_flow_rates.append(average_flow_rate)
+        average_interval_lst.append(average_flow_rate_interval)
 
-    average_flow_rates_min.append(average_flow_rates)
+    average_min_lst.append(average_interval_lst)
 
-print(average_flow_rates)
-print(average_flow_rates_min)
+print(average_min_lst)
 
 # %%
 #f __name__ == "__main__":
