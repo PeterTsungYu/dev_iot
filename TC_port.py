ffe77d4493d7f11769225e0669e49dd768d21555
Peter_PC <yoyo5022@gmail.com>
2021-01-28 00:19:13 +0800

    modify threading ...

--- TC_port.py
+++ TC_port.py
@@ -24,83 +24,77 @@ ser.writeTimeout = 0.5     #timeout for write 0.5s
 #-----------------ModBus RTU------------------------------
 RTU1 = Modbus.RTU('03', '03', '0000', '0008')
 
-def gen_Slave():
-    slave_3 = Modbus.Slave('03', RTU1.rtu)
+def gen_Slave(RTU):
+    slave_3 = Modbus.Slave(RTU.id, RTU.rtu)
     return slave_3
-
-slave_3 = gen_Slave()
-print(slave_3.rtu)
+slave_3 = gen_Slave(RTU1)
+#print(slave_3.rtu)
 print('Generate Slaves: succeed')
 #------------------------------------------------------------------
 # %%
-def buffer_check():
-    # set initial event is False
-    if read_state.isSet(): 
-        read_state.clear() # read, wait
-    if write_state.isSet(): 
-        write_state.set() # write, first       
+# define Threads and Events...
+
+def buffer_check():    
     while True:
     # look up the buffer for reading
         if ser.in_waiting == (int(RTU1.data_len) * 2): # input has data with required length 
-            read_state.set()
-            write_state.set()
+            buffer_state.set()
 
-def ReadSlave():
+def ReadAdam():
+    start_w = time.time()
     while True:
-        read_state.wait()
-        print('read')
-        print(ser.in_waiting)
-        return_data = ser.read(ser.in_waiting).hex() # return as string
-        slave_3.lst_readings.append(return_data)
-        #ser.reset_input_buffer() 
-        read_state.clear()
+        while ticker.wait(WAIT_TIME_SECONDS): # execute the following while block for a countdown, and then end the loop
+            ser.write(bytearray.fromhex(slave_3.rtu[0])) #hex to binary(byte) 
+            slave_3.time_readings.append(time.time()-start_w)
+            print(f"writing to slave_{slave_3.id}")
 
-def WriteSlave():
-    while True:
-        if start_w == 0:
-            start_w = time.time()
+            buffer_state.wait() # wait for the buffer_check to set the flag 
+            print(f"Reading from slave_{slave_3.id}")
+            print(ser.in_waiting)
+            return_data = ser.read(ser.in_waiting).hex() # return as string
+            slave_3.lst_readings.append(return_data)
+            #ser.reset_input_buffer() 
+            buffer_state.clear()        
         
-        write_state.wait()
-        ser.write(bytearray.fromhex(slave_3.rtu[0])) #hex to binary(byte) 
-        slave_3.time_readings.append(time.time()-start_w)
-        print(f"writing to slave_{slave_3.id}")
-        write_state.clear()
+        # calc after every min, code here...
+        readings = np.array([[reading[i-2:i] for i in range(4+2,len(reading)-2,2)]for reading in slave_3.lst_readings])
+        slave_3.arr_readings = np.sum(readings, axis=0) / len(slave_3.lst_readings)
+        slave_3.time_readings[-1]
+        ## code here...
 
-read_state = threading.Event()
-write_state = threading.Event()
-reader = threading.Thread(target=ReadSlave)
-writer = threading.Thread(target=WriteSlave)
-valve = threading.Thread(target=buffer_check)
+        # flush all data in attributes in slave_3 
+        slave_3 = gen_Slave(RTU1)
+
+
+buffer_state = threading.Event()
+buffer_state.clear() # set initial event is False
+buffer_gate = threading.Thread(target=buffer_check)
+Adam_TC_reader = threading.Thread(target=ReadAdam)
+
+WAIT_TIME_SECONDS = 60 # per min
+ticker = threading.Event()
 # %%
+# open ports on RPi
 try: 
     # open the port
     ser.open()
     ser.reset_input_buffer() #flush input buffer
     ser.reset_output_buffer() #flush output buffer
-
 except Exception as ex:
     print ("open serial port error " + str(ex))
     exit()
 
-start_w = 0
-count = 0
-chunk = 10
-nap = 1
+# %%
+# Main thread...
+
 # flow_rate = 18 # [g/min]
 # steady-state recording 
+buffer_gate.start()
+Adam_TC_reader.start()
 
-# %%
-valve.start()
-reader.start()
-writer.start()
-
-for i in range(5):
+while True: # execute the following after every min countdown
     try:
-        print('round complete')
-
-        # flush all data in attributes in slave_3 
-        #slaves = gen_Slave()
-
+        print('GUI will be here')
     except Exception as e1:
         print ("communicating error " + str(e1))
 
@@ -115,4 +109,3 @@ print('end')
 # %%
 # verify the exported file
 #np.load('./slave_1_readings.npy')
-# %%
