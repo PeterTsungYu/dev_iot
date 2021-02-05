a76b90e47eba2d3a0ea2c042317e0e545194b123
Peter_Rpi <yoyo5022@gmail.com>
2021-02-05 17:31:53 +0800

    Ready to create reading_threads...

--- ServerRpi.py
+++ ServerRpi.py
@@ -1,7 +1,6 @@
 import serial
 import time
 
-'''
 receiver = serial.Serial(     
      port='/dev/ttyUSB0',        
      baudrate = 115200,
@@ -11,112 +10,18 @@ receiver = serial.Serial(
      timeout=1
      )
 
-while True:
-      x = receiver.readline()
-      print x
+'''
+TC - 8 * 2 = 16 (USB)
+DFM - 1 * 2 = 2 (GPIO)
+GA - 6 * 2 = 12 (USB)
+MFC - 1 * 2 = 2 (GPIO ??)
+Scale - 1 * 2 = 2 (USB)
+Total = 34 (0x22)
+RTU: 06 03 00 00 00 22 C4 64
+each data entry would have (0x00-0xffff, which is 0-65535)
 '''
 
-#!/usr/bin/env python
-from pymodbus.server.sync import ModbusSerialServer
-from pymodbus.datastore import ModbusSequentialDataBlock
-from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-from pymodbus.transaction import ModbusRtuFramer
-import threading
-# --------------------------------------------------------------------------- #
-# configure the service logging
-# --------------------------------------------------------------------------- #
-import logging
-FORMAT = ('%(asctime)-15s %(threadName)-15s'
-          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
-logging.basicConfig(format=FORMAT)
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
-
-def gen_and_run_server():
-    register_block = ModbusSequentialDataBlock(0x00, [0]*0xff)
-    #print(register_block.__str__())
-
-    store = ModbusSlaveContext(
-        #di=ModbusSequentialDataBlock(0, [1]*100),
-        #co=ModbusSequentialDataBlock(0, [2]*100),
-        hr=register_block, # holding register block, for func = 3, 6, 16
-        #ir=ModbusSequentialDataBlock(0, [4]*100),
-        zero_mode=True
-        )
-    #print(store.getValues(fx=3, address=0x00, count=5))
-
-    context = ModbusServerContext(
-        slaves={slave_id:store,}, # collection of slaves; here only slave 6
-        single=False
-        )
-    #print(context)    
-    #print(f"This is the slave 6: {context[slave_id]}") # index as a dict; otherwise, as a list
-
-    # RTU:
-    RTU_server = ModbusSerialServer(
-        context, 
-        framer=ModbusRtuFramer,  
-        port='/dev/ttyS0', 
-        timeout=1, 
-        baudrate=115200,
-        stopbits=1,
-        bytesize=8,
-        parity='N', 
-        )
-
-    return RTU_server
-
-
-def updating_writer(context):
-    #print(context[slave_id])
-    log.debug("updating the context")
-    values = context[slave_id].getValues(fx=fx_code, address=address, count=5)
-    values = [v + 1 for v in values]
-    log.debug("new values: " + str(values))
-    context[slave_id].setValues(fx=fx_code, address=address, values=values)
-
-
-class update_and_timeout(threading.Thread):
-    def __init__(self, context):
-        threading.Thread.__init__(self)
-        self._timeout = threading.Event()
-        self.context = context
-        print(self.context)
-
-    def run(self):
-        while not self._timeout.wait(1): # every beginning of iteration, timeout for 1s
-            updating_writer(self.context)
-
-    def join(self, timeout=None):
-        """ Stop the thread. """
-        self._timeout.set(  )
-        threading.Thread.join(self, timeout)
-
-
-if __name__ == "__main__":
-    fx_code = 3
-    slave_id = 0x06
-    address = 0x10
-    try:
-        Rpi = gen_and_run_server()
-        print("Server is online")
-
-        update = update_and_timeout(Rpi.context)
-        update.start()
-        time.sleep(600.0)
-        update.join()
-        '''
-        while True:
-            run_updating_server = threading.Timer(interval=1, function=updating_writer, args=Rpi.context)
-            run_updating_server.start()
-            run_updating_server.join()
-        '''
-
-    except Exception as ex:
-        print ("open serial port error " + str(ex))
-        Rpi.server_close()
-        print("Server is offline")
-
-
-print(Rpi.context[0x06].getValues(fx=fx_code, address=address, count=5))
-    
+request = '060300000022C464'
+while True:
+    if str(receiver.read(16).hex()) == request:
+    time.sleep(1)
