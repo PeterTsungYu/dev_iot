e0993d8f78a253f0dfae6a11c4af1a0caec74d6a
Peter_Rpi <yoyo5022@gmail.com>
2021-02-09 18:15:07 +0800

    All Threading test...

--- Modbus.py
+++ Modbus.py
@@ -3,9 +3,9 @@ import numpy as np
 import serial
 import time
 import threading
+import re
 
-# RTU
-#------------------------------------------------------------------
+#-------------------------RTU & Slave--------------------------------------
 class RTU: # generate the CRC for the complete RTU 
     def __init__(self, idno='', func_code='', data_site='', data_len=''):
         self.id = idno # id number of slave
@@ -28,11 +28,8 @@ def gen_Slave(RTU): # deprecated
     slave = Slave(RTU.id, RTU.rtu)
     return slave
 '''
-#------------------------------------------------------------------
 
-
-# threading
-#------------------------------------------------------------------
+#--------------------------Threading--------------------------------
 '''
 class SlaveThread(threading.Thread): # deprecated 
     def __init__(self, name='SlaveThread'):
@@ -53,38 +50,102 @@ def terminate(event): # ask user input to stop the program
     text = input("Type 'exit' to terminate the program...\n>")
     if text == 'exit':
         event.set()
-#------------------------------------------------------------------
 
-
-# func
-#------------------------------------------------------------------
+#------------------------------func---------------------------------
 def Adam_data_collect(kb_event, port, slave, start, time_out, wait_data):
     while not kb_event.isSet():
         try:
             port.write(bytes.fromhex(slave.rtu)) #hex to binary(byte) 
-            slave.time_readings.append(time.time()-start)
+            slave.time_readings.append(round(time.time()-start, 2))
 
             time.sleep(time_out)
 
             # look up the buffer for 21 bytes, which is for 8 channels data length
             if port.inWaiting() == wait_data: 
                 readings = port.read(wait_data).hex()
-                print(readings)
+                #print(readings)
                 slave.lst_readings.append(readings)
             else: # if data is not correct, return as None
                 slave.lst_readings.append(None)
                 port.reset_input_buffer() 
-            print('collect done')
         except Exception as e1:
-            print ("Adam_data_collect error " + str(e1))
+            print ("Adam_data_collect error: " + str(e1))
+        finally:
+            port.reset_input_buffer() # reset the buffer after each reading process
+            print('Adam_data_collect: done')
     port.close()
     print('kill Adam_data_collect')
 
 
-def Adam_data_analyze(kb_event, ticker, wait_time, slave):
+def Scale_data_collect(kb_event, port, slave, start, time_out):
     while not kb_event.isSet():
         try:
-            if not ticker.wait(wait_time):
+            time.sleep(time_out) # wait for the data input to the buffer
+            if port.inWaiting():
+                readings = port.read(port.inWaiting()).decode('utf-8')
+                slave.time_readings.append(round(time.time()-start, 2))
+                readings = [float(s) if s[0] != '-' else -float(s[1:]) for s in re.findall(r'[ \-][ \d]{5}\.\d', readings)]
+                slave.lst_readings.append(readings) 
+        except Exception as e1:
+            print ("Scale_data_collect error: " + str(e1))
+        finally:
+            port.reset_input_buffer() # reset the buffer after each reading process
+            print('Scale_data_collect done')
+    port.close()
+    print('kill Scale_data_collect')
+
+
+def MFC_data_collect(port, slave, start, time_out, wait_data):
+    #while not kb_event.isSet():
+    try:
+        port.write(bytes(slave.rtu, 'utf-8')) #string to binary(byte) 
+        slave.time_readings.append(round(time.time()-start, 2))
+
+        time.sleep(time_out)
+        
+        print(port.inWaiting())
+        if port.inWaiting() == wait_data:
+            readings = port.read(port.inWaiting()).decode('utf-8')
+            print(readings)
+            slave.lst_readings.append(readings)
+    except Exception as e1:
+        print ("MFC_data_collect error: " + str(e1))
+    finally:
+        port.reset_input_buffer() # reset the buffer after each reading process
+        print('MFC_data_collect done')
+    #port.close()
+    #print('kill MFC_data_collect')
+
+
+def GA_data_collect(port, slave, start, time_out, wait_data):
+    #while not kb_event.isSet():
+    try:
+        port.write(bytes.fromhex(slave.rtu)) #hex to binary(byte) 
+        slave.time_readings.append(time.time()-start)
+
+        time.sleep(time_out)
+
+        #print(port.inWaiting())
+        if port.inWaiting() == wait_data: 
+            readings = port.read(wait_data).hex()
+            #print(len(readings))
+            slave.lst_readings.append(readings)
+        else: # if data is not correct, return as None
+            slave.lst_readings.append(None)
+            port.reset_input_buffer() 
+    except Exception as e1:
+        print ("GA_data_collect error: " + str(e1))
+    finally:
+        port.reset_input_buffer() # reset the buffer after each reading process
+        print('GA_data_collect done')
+    #port.close()
+    #print('kill MFC_data_collect')
+
+
+def Adam_data_analyze(kb_event, ticker, sample_time, slave):
+    while not kb_event.isSet():
+        try:
+            if not ticker.wait(sample_time):
                 lst_readings = slave.lst_readings
                 time_readings = slave.time_readings
                 slave.lst_readings = []
@@ -97,19 +158,17 @@ def Adam_data_analyze(kb_event, ticker, wait_time, slave):
                 readings.append(round(time_readings[-1],2))
                 readings.append(lst_readings)
                 slave.readings.append(readings)
-                print('analyze done')
+                print(f'Adam_data_analyze done: {readings}')
         except Exception as e1:
-            print ("Adam_data_analyze error " + str(e1))
+            print ("Adam_data_analyze error: " + str(e1))
     print('kill Adam_data_analyze')
-    print(slave.lst_readings)
-    print(slave.time_readings)
-    print(slave.readings)
+    print(f'Final Adam_data_analyze: {slave.readings}')
 
 
-def DFM_data_analyze(kb_event, ticker, wait_time, slave):
+def DFM_data_analyze(kb_event, ticker, sample_time, slave):
     while not kb_event.isSet():
         try:
-            if not ticker.wait(wait_time): # for each wait_time, collect data 
+            if not ticker.wait(sample_time): # for each sample_time, collect data 
                 time_readings = slave.time_readings
                 slave.time_readings = []
 
@@ -128,11 +187,95 @@ def DFM_data_analyze(kb_event, ticker, wait_time, slave):
                             print("DFM_data_analyze internal loop Error: " + str(ex))
                     average_flow_rate_interval = round(sum(flow_rate_interval_lst) / len(flow_rate_interval_lst), 2)          
                     average_interval_lst.append(average_flow_rate_interval)
-                slave.readings.append(round(sum(average_interval_lst) / len(average_interval_lst), 2))
-                
+                    _average = round(sum(average_interval_lst) / len(average_interval_lst), 2)
+                slave.readings.append(_average)
+                print(f'DFM_data_analyze done: {_average}')
         except Exception as e1:
-            print ("DFM_data_analyze error " + str(e1))
-    print('kill Adam_data_analyze')
-    print(slave.lst_readings)
-    print(slave.time_readings)
-    print(slave.readings)
+            print ("DFM_data_analyze error: " + str(e1))
+    print('kill DFM_data_analyze')
+    print(f'Final DFM_data_analyze: {slave.readings}')
+
+
+def Scale_data_analyze(kb_event, ticker, sample_time, slave):
+    while not kb_event.isSet():
+        try:
+            if not ticker.wait(sample_time):
+                lst_readings = slave.lst_readings
+                time_readings = slave.time_readings
+                slave.lst_readings = []
+                slave.time_readings = []
+
+                lst_readings = [sum(i)/len(i) for i in lst_readings] # average for 1s' data
+                lst_readings = round(sum(lst_readings) / len(lst_readings), 1) # average for 1min's data
+
+                readings = []
+                readings.append(time_readings[-1])
+                readings.append(lst_readings)
+                slave.readings.append(readings)
+                print(f'Scale_data_analyze done: {readings}')
+        except Exception as ex:
+            print ("Scale_data_analyze error: " + str(ex))
+    print('kill Scale_data_analyze')
+    print(f'Final Scale_data_analyze: {slave.readings}')
+
+
+def GA_data_analyze(kb_event, ticker, sample_time, slave):
+    while not kb_event.isSet():
+        try:
+            if not ticker.wait(sample_time):
+                lst_readings = slave.lst_readings
+                time_readings = slave.time_readings
+                slave.lst_readings = []
+                slave.time_readings = []
+
+                arr_readings = np.array(
+                    [[int(readings[i:i+4],16)/100 for i in range(8,20,4)] 
+                    + [int(readings[24:28],16)/100] 
+                    + [int(readings[-12:-8],16)/100] 
+                    + [(lambda i: ((i[0]*256+i[1]+i[2])*256+i[3])/100)([int(readings[i:i+2],16) for i in range(-20,-12,2)])] 
+                    for readings in lst_readings]
+                    )
+                #print(arr_readings)
+                lst_readings = list(np.round(np.sum(arr_readings, axis=0) / len(lst_readings), 2))
+
+                readings = []
+                readings.append(time_readings[-1])
+                readings.append(lst_readings)
+                slave.readings.append(readings)
+                print(f'GA_data_analyze done: {readings}')
+        except Exception as ex:
+            print ("GA_data_analyze error: " + str(ex))
+    print('kill GA_data_analyze')
+    print(f'Final GA_data_analyze: {slave.readings}')
+
+
+def MFC_data_analyze(kb_event, ticker, sample_time, slave):
+    while not kb_event.isSet():
+        try:
+            if not ticker.wait(sample_time):
+                lst_readings = slave.lst_readings
+                time_readings = slave.time_readings
+                slave.lst_readings = []
+                slave.time_readings = []
+
+                arr_readings = np.array(
+                    [
+                        (lambda i: [float(s) if s[0] != '-' else -float(s[1:]) for s in re.findall(r'[ +\-][\d.]{6}', i)])(readings) 
+                        for readings in lst_readings
+                    ]
+                    )
+                #print(arr_readings)
+                lst_readings = tuple(np.round(np.sum(arr_readings, axis=0) / len(lst_readings), 2))
+                readings = tuple(time_readings[-1:]) + lst_readings
+                '''
+                conn.execute(
+                    "INSERT INTO MFC(Time, Pressure, Temper, VolFlow, MassFlow, Setpoint) VALUES (?,?,?,?,?,?);", 
+                    readings
+                    )
+                '''
+                slave.readings.append(readings)
+                print(f'MFC_data_analyze done: {readings}')
+        except Exception as ex:
+            print ("MFC_data_analyze error: " + str(ex))
+    print('kill MFC_data_analyze')
+    print(f'Final MFC_data_analyze: {slave.readings}')
--- ReadingThreads.py
+++ ReadingThreads.py
@@ -10,11 +10,10 @@ import re
 import sqlite3
 conn = sqlite3.connect('./SQlite/test_DB.db')
 
-
 print('Import: succeed')
 
-# %%
 #-----------------Serial port setting------------------------------
+lst_port = []
 ## device ID
 ADAM_TC_id = '03'
 GA_id = '11'
@@ -22,13 +21,27 @@ MFC_id = 'a'
 
 ## RS485
 ### set the baudrate of ADAM to 19200
-RS485_port = serial.Serial(port=input('RS485 port: '), baudrate=19200, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE, parity=PARITY_NONE)
+RS485_port = serial.Serial(
+    port='/dev/ttyUSB0', # input('RS485 port: ') 
+    baudrate=19200, 
+    bytesize=8, 
+    stopbits=1, 
+    parity='N'
+    )
+lst_port.append(RS485_port)
 ADAM_TC_RTU = Modbus.RTU(ADAM_TC_id, '03', '0000', '0008')
 ADAM_TC_slave = Modbus.Slave(ADAM_TC_id, ADAM_TC_RTU.rtu)
 
 ## RS232
 ### set the baudrate of GA & MFC to 9600
-RS232_port = serial.Serial(port=input('RS232 port: '), baudrate=9600, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE, parity=PARITY_NONE)
+RS232_port = serial.Serial(
+    port='/dev/ttyUSB1', # input('RS232 port: ') 
+    baudrate=9600, 
+    bytesize=8, 
+    stopbits=1, 
+    parity='N'
+    )
+lst_port.append(RS232_port)
 GA_RTU = '11 01 60 8E'
 GA_slave = Modbus.Slave(GA_id, GA_RTU)
 
@@ -36,24 +49,34 @@ MFC_RTU = MFC_id + '\r'
 MFC_slave = Modbus.Slave(MFC_id, MFC_RTU)
 
 # Scale USB
-Scale_port = serial.Serial(port=input('Scale port: '), baudrate=9600, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE, parity=PARITY_NONE)
+Scale_port = serial.Serial(
+    port='/dev/ttyUSB2', # input('Scale port: ') 
+    baudrate=9600, 
+    bytesize=8, 
+    stopbits=1, 
+    parity='N'
+    )
+lst_port.append(Scale_port)
 Scale_slave = Modbus.Slave()
 
-#-----GPIO port setting------------------------------
+#-----GPIO port setting----------------------------------------------------------------
 ## DFM
 # read High as 3.3V
 channel_DFM = 18
 GPIO.setmode(GPIO.BCM)
 DFM_slave = Modbus.Slave()
 
-
-print('Serial port setting: succeed')
+print('Port setting: succeed')
 
 #-------------------------define Threads and Events-----------------------------------------
-# count down events
+start = time.time()
+sample_time = 30
+lst_thread = []
+
+## count down events
 ticker = threading.Event()
 
-# Keyboard interrupt to kill all the threads
+## Keyboard interrupt event to kill all the threads
 kbinterrupt_event = threading.Event()
 def signal_handler(signum, frame):
     kbinterrupt_event.set()
@@ -62,47 +85,99 @@ signal.signal(signal.SIGINT, signal_handler) # Keyboard interrupt to stop the pr
 ## RS485
 Adam_data_collect = threading.Thread(
     target=Modbus.Adam_data_collect, 
-    args=(kbinterrupt_event, ser, slave_3, start, 1, 21,),
+    args=(kbinterrupt_event, RS485_port, ADAM_TC_slave, start, 1, 21,),
     )
 Adam_data_analyze = threading.Thread(
     target=Modbus.Adam_data_analyze, 
-    args=(kbinterrupt_event, ticker, 30, slave_3,),
+    args=(kbinterrupt_event, ticker, sample_time, ADAM_TC_slave,),
     )
+lst_thread.append(Adam_data_collect)
+lst_thread.append(Adam_data_analyze)
+
+## RS232
+def RS232_data_collect(kb_event, port):
+    while not kb_event.isSet():
+        Modbus.GA_data_collect(port, GA_slave, start, 1, 31)
+        Modbus.MFC_data_collect(port, MFC_slave, start, 1, 49)
+    port.close()
+    print('kill GA_data_collect')
+    print('kill MFC_data_collect')
+RS232_data_collect = threading.Thread(
+    target=RS232_data_collect, 
+    args=(kbinterrupt_event, RS232_port,)
+    )
+GA_data_analyze = threading.Thread(
+    target=Modbus.GA_data_analyze, 
+    args=(kbinterrupt_event, ticker, sample_time, GA_slave,),
+    )
+MFC_data_analyze = threading.Thread(
+    target=Modbus.MFC_data_analyze, 
+    args=(kbinterrupt_event, ticker, sample_time, MFC_slave,),
+    )
+lst_thread.append(RS232_data_collect)
+lst_thread.append(GA_data_analyze)
+lst_thread.append(MFC_data_analyze)
+
+
+## Scale USB
+Scale_data_collect = threading.Thread(
+    target=Modbus.Scale_data_collect, 
+    args=(kbinterrupt_event, Scale_port, Scale_slave, start, 1,),
+    )
+Scale_data_analyze = threading.Thread(
+    target=Modbus.Scale_data_analyze, 
+    args=(kbinterrupt_event, ticker, sample_time, Scale_slave,),
+    )
+lst_thread.append(Scale_data_collect)
+lst_thread.append(Scale_data_analyze)
 
 ## GPIO
 # Edge detection
-def DFM_data_collect():
+def DFM_data_collect(channel_DFM):
     DFM_slave.time_readings.append(time.time())
+DFM_data_analyze = threading.Thread(
+    target=Modbus.DFM_data_analyze, 
+    args=(kbinterrupt_event, ticker, sample_time, DFM_slave,),
+    )
+lst_thread.append(DFM_data_analyze)
 
 #-------------------------Open ports--------------------------------------
-try: 
-    ser.open() # code here........
-    ser.reset_input_buffer() #flush input buffer
-    ser.reset_output_buffer() #flush output buffer
-
+try:
+    for port in lst_port: 
+        if not port.is_open:
+            port.open() # code here........
+        port.reset_input_buffer() #flush input buffer
+        port.reset_output_buffer() #flush output buffer
+    print('serial ports open')
+    
     GPIO.setup(channel_DFM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    print('GPIO ports open')
     
 except Exception as ex:
-    print ("open serial port error " + str(ex))
-    exit()
+    print ("open serial port error: " + str(ex))
+    for port in lst_port: 
+        port.close()
+    exit() 
 
-#-------------------------Threading-----------------------------------------
-start = time.time()
-flag_time = start
-Adam_data_collect.start()
-Adam_data_analyze.start()
+#-------------------------Sub Threadingggg-----------------------------------------
+for subthread in lst_thread:
+    subthread.start()
+
 GPIO.add_event_detect(channel_DFM, GPIO.RISING, callback=DFM_data_collect)
 
+#-------------------------Main Threadingggg-----------------------------------------
 try:
     while not kbinterrupt_event.isSet():
+        if not ticker.wait(sample_time):
+            print(f'Sampling at {time.time()-start}')
         pass
 except KeyboardInterrupt: 
-    GPIO.cleanup() 
-    print(f"Program duration: {time.time() - start}")
-    print(f"Keyboard Interrupt...")
+    print(f"Keyboard Interrupt in main thread!")
     print("=="*30)
 except Exception as ex:
-    print ("communicating error " + str(ex))    
+    print ("Main threading error: " + str(ex))    
     print("=="*30)
 finally:
+    GPIO.cleanup() 
+    print(f"Program duration: {time.time() - start}")
     print('kill main thread')
