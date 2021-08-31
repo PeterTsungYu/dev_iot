b835a01f2f5765828dab79404f940b8444c1ed99
rasp-001 <yoyo5022@gmail.com>
2021-08-31 16:24:57 +0800

    1. Refactored threading funcs in Threads_rasp*.py
    TODO:
    1. Testing
    2. Refactor NodeRed UI and connection

--- Threads_rasp-001.py
+++ Threads_rasp-001.py
@@ -2,143 +2,31 @@
 # python packages
 import time
 from datetime import datetime
-import threading
 import RPi.GPIO as GPIO
 
 # custome modules
 import params
-import Modbus
 import config
 import MQTT_config
 import Mariadb_config
 
 print('Import: succeed')
 
-#-----port and slave setting----------------------------------------------------------------
-lst_port = []
-lst_port.append(config.RS485_port)
-lst_port.append(config.RS232_port)
-lst_port.append(config.Scale_port)
-lst_port.append(config.Setup_port)
-
-#-------------------------define Threads and Events-----------------------------------------
+#-------------------------Start line-----------------------------------------
 timeit = datetime.now().strftime('%Y_%m_%d_%H_%M')
 print(f'Execution time is {timeit}')
 print('=='*30)
 start = time.time()
 
-# RS485_port
-ADAM_TC_collect = threading.Thread(
-    target=Modbus.ADAM_TC_collect, 
-    args=(start, config.RS485_port, ADAM_TC_slave, 21,),
-    )
-ADAM_TC_analyze = threading.Thread(
-    target=Modbus.ADAM_TC_analyze, 
-    args=(start, ADAM_TC_slave,),
-    )
-
-## RS232_port
-RS232_data_collect = threading.Thread(
-    target=Modbus.RS232_data_collect, 
-    args=(config.RS232_port,)
-    )
-GA_data_analyze = threading.Thread(
-    target=Modbus.GA_data_analyze, 
-    args=(start, GA_slave,),
-    )
-
-# Scale_port
-Scale_data_collect = threading.Thread(
-    target=Modbus.Scale_data_collect, 
-    args=(start, config.Scale_port, Scale_slave, 0,),
-    )
-Scale_data_analyze = threading.Thread(
-    target=Modbus.Scale_data_analyze, 
-    args=(start, Scale_slave, 'Scale',),
-    )
-
-# add READ
-def Setup_data_collect(port):
-    global Header_EVA_collect_err, Header_BR_collect_err, ADAM_SET_collect_err
-    while not params.kb_event.isSet():
-        Header_EVA_collect_err = Modbus.TCHeader_comm(
-            start, port, Header_EVA_slave, 7, Header_EVA_collect_err, 
-            MQTT_config.sub_Topics['Header_EVA_SV']['event'], #todo: config a general slave class
-            MQTT_config.sub_Topics['Header_EVA_SV']['value'], 
-            ) # wait for 7 bytes
-        #print(Header_EVA_collect_err)
-        Header_BR_collect_err = Modbus.TCHeader_comm(
-            start, port, Header_BR_slave, 7, Header_BR_collect_err, 
-            MQTT_config.sub_Topics['Header_BR_SV']['event'], #todo: config a general slave class
-            MQTT_config.sub_Topics['Header_BR_SV']['value'],
-            ) # wait for 7 bytes
-        #print(Header_BR_collect_err)
-        ADAM_SET_collect_err = Modbus.ADAM_SET_comm(
-            start, port, ADAM_SET_slave, 7, ADAM_SET_collect_err, 
-            MQTT_config.sub_Topics['ADAM_SET_SV0']['event'], #todo: config a general slave class
-            MQTT_config.sub_Topics['ADAM_SET_SV0']['value'], 
-            ) # wait for 7 bytes == 7 Hex numbers
-    port.close()
-    print('kill TCHeader_comm')
-    print(f'Final TCHeader_comm: {Header_EVA_collect_err} and {Header_BR_collect_err} errors occured')
-    print('kill ADAM_SET_comm')
-    print(f'Final ADAM_SET_comm: {ADAM_SET_collect_err} errors occured')
-    
-Setup_data_collect = threading.Thread(
-    target=Setup_data_collect, 
-    args=(config.RS485_port,)
-    )
-Header_EVA_analyze = threading.Thread(
-    target=Modbus.TCHeader_analyze, 
-    args=(start, Header_EVA_slave, 'Header_EVA_PV',),
-    )
-Header_BR_analyze = threading.Thread(
-    target=Modbus.TCHeader_analyze, 
-    args=(start, Header_BR_slave, 'Header_BR_PV',),
-    )
-ADAM_SET_analyze = threading.Thread(
-    target=Modbus.ADAM_SET_analyze, 
-    args=(start, ADAM_SET_slave, 'ADAM_SET_PV0',),
-    )
-# add READ func
-
-## GPIO_port
-# Edge detection
-def DFM_data_collect():
-    DFM_slave.time_readings.append(time.time())
-DFM_data_analyze = threading.Thread(
-    target=Modbus.DFM_data_analyze, 
-    args=(start, DFM_slave,),
-    )
-
-def DFM_AOG_data_collect():
-    DFM_AOG_slave.time_readings.append(time.time())
-DFM_AOG_data_analyze = threading.Thread(
-    target=Modbus.DFM_AOG_data_analyze, 
-    args=(start, DFM_AOG_slave,),
-    )
-
-lst_thread = []
-lst_thread.append(ADAM_TC_collect)
-lst_thread.append(ADAM_TC_analyze)
-lst_thread.append(RS232_data_collect)
-lst_thread.append(GA_data_analyze)
-lst_thread.append(Scale_data_collect)
-lst_thread.append(Scale_data_analyze)
-lst_thread.append(Setup_data_collect)
-lst_thread.append(Header_EVA_analyze)
-lst_thread.append(Header_BR_analyze)
-lst_thread.append(ADAM_SET_analyze)
-lst_thread.append(DFM_data_analyze)
-lst_thread.append(DFM_AOG_data_analyze)
-
 #-------------------------Open ports--------------------------------------
+lst_ports = [config.RS485_port, config.Scale_port, config.RS232_port, config.Setup_port]
+
 try:
-    for port in lst_port: 
-        if not port.is_open:
-            port.open() # code here........
-        port.reset_input_buffer() #flush input buffer
-        port.reset_output_buffer() #flush output buffer
+    for device_port in lst_ports: 
+        if not device_port.port.is_open:
+            device_port.port.open() # code here........
+        device_port.port.reset_input_buffer() #flush input buffer
+        device_port.port.reset_output_buffer() #flush output buffer
     print('serial ports open')
     
     GPIO.setup(config.channel_DFM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -147,16 +35,36 @@ try:
     
 except Exception as ex:
     print ("open serial port error: " + str(ex))
-    for port in lst_port: 
-        port.close()
+    for device_port in lst_ports: 
+        device_port.port.close()
     exit() 
 
-#-------------------------Sub Threadingggg-----------------------------------------
-for subthread in lst_thread:
-    subthread.start()
+#-------------------------Sub-Threadingggg-----------------------------------------
+try:
+    for device_port in lst_ports: 
+        device_port.serial_funcs(start)
+        device_port.parallel_funcs(start) 
+        for subthread in device_port.thread_funcs:
+            subthread.start()
+    print('Threading funcs')
+           
+except Exception as ex:
+    print ("Threading funcs error: " + str(ex))
+    for device_port in lst_ports:
+            device_port.port.close()
+    exit() 
 
+#-------------------------GPIO Threadingggg-----------------------------------------
+def DFM_data_collect():
+    config.DFM_slave.time_readings.append(time.time())
+def DFM_AOG_data_collect():
+    config.DFM_AOG_slave.time_readings.append(time.time())
 GPIO.add_event_detect(config.channel_DFM, GPIO.RISING, callback=DFM_data_collect)
 GPIO.add_event_detect(config.channel_DFM_AOG, GPIO.RISING, callback=DFM_AOG_data_collect)
+config.GPIO_port.serial_funcs(start)
+config.GPIO_port.parallel_funcs(start)
+for subthread in config.GPIO_port.thread_funcs:
+    subthread.start()
 
 #-------------------------Main Threadingggg-----------------------------------------
 try:
