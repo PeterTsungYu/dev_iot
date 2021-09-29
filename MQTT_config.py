2fb1f68dfdb08b7c15eb07cf995c50bbcdda167e
rasp-001 <yoyo5022@gmail.com>
2021-09-29 18:06:25 +0800

    1. Impair the issue of continuous connecting MQTT 2. Modify node red

--- MQTT_config.py
+++ MQTT_config.py
@@ -3,27 +3,24 @@
 #python packages
 import paho.mqtt.client as mqtt
 import threading
-#import time 
+import json
 
 #custom modules
 import params
 import config
 
-#-------------------------MQTT sub/pub--------------------------------------
-lst_port_Topics = [config.RS485_port, config.RS232_port, config.Scale_port, config.Setup_port, config.GPIO_port]
-
 #-------------------------MQTT func--------------------------------------
 def connect_mqtt(client_id, hostname='localhost', port=1883, keepalive=60,):
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print(f"Connected to MQTT!" + f">>> {client_id}")
+            print(f"Connected to MQTT!")
         else:
-            print(f"{client_id} Failed to connect, return code %d\n", rc)
+            print(f"Failed to connect, return code %d\n", rc)
         
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        client.subscribe("nodered", qos=0)
-        client.subscribe([(u,0) for i in lst_port_Topics for u in i.sub_topics])
+        #client.subscribe("nodered", qos=0)
+        client.subscribe([(u,0) for i in config.lst_ports for u in i.sub_topics])
         #client.subscribe([("Header_EVA_SV", 0), ("Header_BR_SV", 0)])
 
     # The callback for when a SUB message is received
@@ -38,7 +35,8 @@ def connect_mqtt(client_id, hostname='localhost', port=1883, keepalive=60,):
 
     client = mqtt.Client(client_id=client_id, clean_session=True)
     # client.username_pw_set(username, password)
-    client.on_connect = on_connect
+    #client.on_connect = on_connect
+    client.subscribe([(u,0) for i in config.lst_ports for u in i.sub_topics])
     client.on_message = on_message
     #client.on_publish = on_publish
     client.connect_async(hostname, port, keepalive)
@@ -48,15 +46,16 @@ def connect_mqtt(client_id, hostname='localhost', port=1883, keepalive=60,):
 def multi_pub(client):
     while not params.kb_event.isSet():
         if not params.ticker.wait(params.sample_time):
-            #print(pub_Topics)
-            for port_topic in lst_port_Topics:
-                for key, value in port_topic.pub_values.items():
-                    client.publish(topic=key, payload=value, qos=0, retain=True)
-            print(f"pub succeed {client._client_id} >>> localhost")
+            for device_port in config.lst_ports:
+                payload = {}
+                for key, value in device_port.pub_values.items():
+                    payload[key] = value
+                payload = json.dumps(payload)
+                client.publish(topic=f"{device_port.name}", payload=payload, qos=0, retain=True)
+                print(f"pub f'{device_port.name}_pub':{payload} succeed from {client._client_id} >>> localhost")
 
 #-------------------------MQTT instance--------------------------------------
-client_0 = connect_mqtt(
-    client_id='client0', hostname='localhost', port=1883, keepalive=60,) 
+client_0 = connect_mqtt(client_id='client_0' ,hostname='localhost', port=1883, keepalive=60,) 
 client_0.loop_start()
 
 multi_pub = threading.Thread(
