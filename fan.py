6db299c754161776225286dc0ce19feb9d5acdc3
rasp-001 <yoyo5022@gmail.com>
2022-08-30 10:33:04 +0800

    Add signal to fan.py

--- fan.py
+++ fan.py
@@ -1,5 +1,17 @@
 import pigpio
 import time
+import threading
+import signal
+
+kb_event = threading.Event()
+def signal_handler(signum, frame):
+    if kb_event.is_set():
+        kb_event.clear()
+        kb_event.set()
+    else:
+        kb_event.set()
+signal.signal(signal.SIGINT, signal_handler) # Keyboard interrupt to stop the program
+
 
 PIG = pigpio.pi()
 
@@ -12,12 +24,12 @@ PIG.set_pull_up_down(channel, pigpio.PUD_UP)
 def get_temp():
     with open('/sys/class/thermal/thermal_zone0/temp') as fp:
         return int(fp.read()) / 1000
- 
-while True:
+
+while not kb_event.is_set():
     temp = get_temp()
     if temp >= upper_temp:
         PIG.write(channel, 0)
     elif temp < lower_temp:
         PIG.write(channel, 1)
-    time.sleep(10)
+    time.sleep(5)
 PIG.stop()
